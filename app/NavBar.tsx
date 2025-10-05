6b62ce975bcf20d43769e4469efa6782d4ab64b8
Elamuruhu <elamuruhu@juzgodigital.com>
2025-10-05 22:01:34 +0530

    styled nav links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react';
+import { usePathname } from 'next/navigation';
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+    const currentPath = usePathname();
+    console.log("currentPath",currentPath);
     const links = [
         {label : 'Dashboard' , href : '/'},
         {label : 'Issues' , href : '/issues'},
@@ -11,11 +16,17 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><FaBug /></Link>
         <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+            {links.map(link => 
+            <Link 
+                key={link.href} 
+                href={link.href} 
+                className={`${link.href === currentPath ? 'text-zinc-900' :'text-zinc-500'} hover:text-zinc-800 transition-colors`}>
+                {link.label}
+            </Link>)}
            
         </ul>
     </nav>
   )
 }
-
+// className='text-zinc-500 hover:text-zinc-800 transition-colors'
 export default NavBar
